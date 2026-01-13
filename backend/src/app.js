f671c385749759eb731ee521c19d1e673a5cefd1
LucianoSF1992 <lucianosilvaferreira22@gmail.com>
2026-01-13 08:52:01 -0300

    Conexão com banco de dados MySQL

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,14 @@ const cors = require("cors");
 
 const app = express();
 
+// adicionar temporariamente no app.js
+const db = require("./database/connection");
+
+db.query("SELECT 1")
+  .then(() => console.log("MySQL conectado com sucesso"))
+  .catch(err => console.error(err));
+
+
 app.use(cors());
 app.use(express.json());
 
